64372a4fbd77f3ae66c6561c608896b7a01803eb
marcosvandrade <vascorj2006@gmail.com>
2024-08-15 11:15:15 -0300

    melhorando a interface gráfica

--- sprint3.py
+++ sprint3.py
@@ -8,52 +8,180 @@
 # In[6]:
 
 
-# importando a biblioteca do yahoo finance
+# Importando as bibliotecas necessárias
 import yfinance as yf
-
-# importando a biblioteca streamlit e atribuindo o apelido "st"
 import streamlit as st
-
-# bliblioteca responsável pela atualização automática da página web
 from streamlit_autorefresh import st_autorefresh
-
-# importando blibliotecas para exibir gráficos
 import plotly.graph_objs as go
-
-# importando bibliotecas para uso de datas
 from datetime import datetime, timedelta
 
-# Criando uma lista de países e intervalos de tempo
+# Criando listas de países, principais ações e intervalos de tempo
 countries = ["Brazil", "United States"]
 intervals = ["1d", "1wk", "1mo"]
 
+brazil_stocks = [
+    "ABEV3.SA",
+    "B3SA3.SA",
+    "BBAS3.SA",
+    "BBDC3.SA",
+    "BBDC4.SA",
+    "BBSE3.SA",
+    "BEEF3.SA",
+    "BRAP4.SA",
+    "BRFS3.SA",
+    "BRKM5.SA",
+    "BRML3.SA",
+    "BTOW3.SA",
+    "CCRO3.SA",
+    "CIEL3.SA",
+    "CMIG4.SA",
+    "COGN3.SA",
+    "CPFE3.SA",
+    "CPLE6.SA",
+    "CSAN3.SA",
+    "CSNA3.SA",
+    "CVCB3.SA",
+    "CYRE3.SA",
+    "ECOR3.SA",
+    "EGIE3.SA",
+    "ELET3.SA",
+    "ELET6.SA",
+    "EMBR3.SA",
+    "ENBR3.SA",
+    "ENGI11.SA",
+    "EQTL3.SA",
+    "EZTC3.SA",
+    "FLRY3.SA",
+    "GGBR4.SA",
+    "GOAU4.SA",
+    "GOLL4.SA",
+    "HAPV3.SA",
+    "HGTX3.SA",
+    "HYPE3.SA",
+    "IGTA3.SA",
+    "IRBR3.SA",
+    "ITSA4.SA",
+    "ITUB4.SA",
+    "JBSS3.SA",
+    "KLBN11.SA",
+    "LAME4.SA",
+    "LIGT3.SA",
+    "LINX3.SA",
+    "LREN3.SA",
+    "MGLU3.SA",
+    "MRFG3.SA",
+    "MRVE3.SA",
+    "MULT3.SA",
+    "NTCO3.SA",
+    "PCAR3.SA",
+    "PETR3.SA",
+    "PETR4.SA",
+    "QUAL3.SA",
+    "RADL3.SA",
+    "RAIL3.SA",
+    "RENT3.SA",
+    "SANB11.SA",
+    "SBSP3.SA",
+    "SULA11.SA",
+    "SUZB3.SA",
+    "TAEE11.SA",
+    "TIMP3.SA",
+    "TOTS3.SA",
+    "UGPA3.SA",
+    "USIM5.SA",
+    "VALE3.SA",
+    "VIVT3.SA",
+    "VVAR3.SA",
+    "WEGE3.SA",
+    "YDUQ3.SA",
+]
 
-# criando uma função para formatar a data
-def format_date(dt, format="%Y-%m-%d"):
-    return dt.strftime(format)
+us_stocks = [
+    "AAPL",
+    "MSFT",
+    "GOOGL",
+    "AMZN",
+    "FB",
+    "TSLA",
+    "NVDA",
+    "AMD",
+    "NFLX",
+    "BABA",
+    "PYPL",
+    "INTC",
+    "CSCO",
+    "ADBE",
+    "CMCSA",
+    "PFE",
+    "JNJ",
+    "MRNA",
+    "BA",
+    "DIS",
+    "T",
+    "VZ",
+    "WMT",
+    "XOM",
+    "CVX",
+    "PG",
+    "KO",
+    "PEP",
+    "MCD",
+    "NKE",
+    "V",
+    "MA",
+    "JPM",
+    "BAC",
+    "C",
+    "GS",
+    "MS",
+    "AMGN",
+    "GILD",
+    "CRM",
+    "SQ",
+    "UBER",
+    "LYFT",
+    "SPCE",
+    "NKLA",
+    "PLTR",
+    "SNOW",
+    "NIO",
+    "XPEV",
+    "LI",
+    "ARKK",
+    "SPY",
+    "QQQ",
+    "DIA",
+    "IWM",
+    "GME",
+    "AMC",
+    "BB",
+    "NOK",
+    "TLRY",
+    "CGC",
+    "ACB",
+    "ZNGA",
+    "SIRI",
+    "EBAY",
+    "TWTR",
+    "ROKU",
+    "PINS",
+    "UBER",
+    "LYFT",
+]
+
+# Criando um dicionário para associar a lista de ações ao país selecionado
+stock_options = {"United States": us_stocks, "Brazil": brazil_stocks}
 
 
-# função de cache do streamlit para evitar redundância de dados
+# Função de cache do Streamlit para evitar redundância de dados
 @st.cache_data()
-
-# criando a função para consulta do usuário
 def consultar_acao(stock, from_date, to_date, interval):
     return yf.download(
         stock, start=from_date, end=to_date, interval=interval, progress=False
     )
 
 
-# criando uma função para formatar a data
-def format_date(dt, format="%Y-%m-%d"):
-    return dt.strftime(format)
-
-
-# Criando o filtro para o usuário selecionar o intervalo de pesquisa desejado
-start_date = datetime.now() - timedelta(days=30)
-end_date = datetime.now()
-
-
-# função para exiblição dos gráficos
+# Função para exibição dos gráficos
 def plotCandleStick(df, acao="ticket"):
     trace1 = {
         "x": df.index,
@@ -65,10 +193,8 @@ def plotCandleStick(df, acao="ticket"):
         "name": acao,
         "showlegend": False,
     }
-
     data = [trace1]
     layout = go.Layout()
-
     return go.Figure(data=data, layout=layout)
 
 
@@ -77,59 +203,66 @@ def plotCandleStick(df, acao="ticket"):
 # In[7]:
 
 
-# criando a barra lateral da página web com algumas ações pré-selecionadas
+# Aplicando melhorias na barra lateral
 barra_lateral = st.sidebar.empty()
+
+# Atualizando a barra lateral para listar as ações de acordo com o país selecionado
 country_select = st.sidebar.selectbox("Escolha o país:", countries)
-stocks = ["AAPL", "MSFT", "GOOGL"]
+stocks = stock_options[country_select]  # Obtém as ações do país selecionado
 stock_select = st.sidebar.selectbox("Selecione a ação:", stocks)
-from_date = st.sidebar.date_input("Data Inicial:", start_date)
-to_date = st.sidebar.date_input("Data Final:", end_date)
+
+# Criando o filtro para o usuário selecionar o intervalo de pesquisa desejado
+start_date = datetime.now() - timedelta(days=30)
+end_date = datetime.now()
+
+from_date = st.sidebar.date_input("Data Inicial:", start_date.date())
+to_date = st.sidebar.date_input("Data Final:", end_date.date())
+
+# Formata as datas no formato brasileiro (DD/MM/YYYY) apenas para exibição
+formatted_start_date = from_date.strftime("%d/%m/%Y")
+formatted_end_date = to_date.strftime("%d/%m/%Y")
+st.write(f"Data Inicial: {formatted_start_date}")
+st.write(f"Data Final: {formatted_end_date}")
+
+# Criando o restante da barra lateral
 interval_select = st.sidebar.selectbox(
-    "Escolha o intervado de tempo desejado (d - dia wk - semana mo - mês):", intervals
+    "Escolha o intervalo de tempo desejado (d - dia wk - semana mo - mês):", intervals
 )
 carregar_dados = st.sidebar.checkbox("Carregar Dados")
-
-grafico_line = st.empty()
-grafico_candle = st.empty()
 
 
 # In[3]:
 
 
-# Definindo título e cabeçalhos para a Home Page
-st.title("Análise Gráfica de Ações em Tempo Real")
-st.header("Ações")
-st.subheader("Análise Gráfica")
+# Exibindo o título com tamanho de fonte personalizado
+st.markdown(
+    "<h1 style='font-size: 24px;'>Análise Gráfica de Ações em Tempo Real</h1>",
+    unsafe_allow_html=True,
+)
 
 
 # In[4]:
 
 
-# função para definir o tempo de atualização da página que visa permitir
-# a análise em tempo real
+# Função para definir o tempo de atualização da página que visa permitir a análise em tempo real
 count = st_autorefresh(interval=5000, limit=10000, key="fizzbuzzcounter")
 
-# if count == 0:
-#     st.write("Count is zero")
-# elif count % 3 == 0 and count % 5 == 0:
-#     st.write("FizzBuzz")
-# elif count % 3 == 0:
-#     st.write("Fizz")
-# elif count % 5 == 0:
-#     st.write("Buzz")
-# else:
-#     st.write(f"Count: {count}")
-
+# Verificação de datas e exibição de gráficos
 if from_date > to_date:
     st.sidebar.error("A data inicial deve ser menor que a data final")
 else:
     df = consultar_acao(
-        stock_select, format_date(from_date), format_date(to_date), interval_select
+        stock_select,
+        from_date.strftime("%Y-%m-%d"),
+        to_date.strftime("%Y-%m-%d"),
+        interval_select,
     )
+
     try:
         fig = plotCandleStick(df)
+        grafico_candle = st.empty()
         grafico_candle.plotly_chart(fig)
-        # grafico_line.line_chart(df["Close"])
+
         if carregar_dados:
             st.subheader("Data")
             st.dataframe(df)
